84fa2ed67f91ff8ac3ff480f22beadb2be6903b6
migue <migue@gunter>
2021-05-23 17:13:20 +0200

    version dos

---
+++ src/arrays/cambio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays;
+
+/**
+ *
+ * @author migue
+ */
+public class cambio {
+    public static void main(String[] args) {
+        System.out.println("soy una clase nueva");
+    }
+    
+}
