da035b6d05de8dff6921e1fd0b4106b448b33044
migue <migue@gunter>
2021-05-23 16:55:21 +0200

    version alpha

---
+++ src/bucles/bucles.java
@@ -0,0 +1,14 @@
+
+package bucles;
+
+public class bucles {
+    public static void main(String[] args) {
+        System.out.println("Arrays");
+        String [] cosas={"arroz","pollo","skate","boligrafo","tetrahedro"};
+        System.out.println("mostrar contenido");
+        for (String cosa : cosas) {
+            System.out.println(cosa);
+        }
+    }
+    
+}
