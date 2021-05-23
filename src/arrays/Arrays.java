3473364aa7d86290bd069afa39037aff3a53d814
migue <migue@gunter>
2021-05-23 16:54:34 +0200

    version alpha

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,22 @@
+
+package arrays;
+
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int a=1;
+        System.out.println("bucles");
+           System.out.println("FOR =");
+           for (int i=0;i<5;i++){
+               System.out.println(i+"= valor \n");}
+          System.out.println("while =");
+          while (a<=5){
+               System.out.println(a+"=valor de a\n");
+          a++;
+          }
+          
+    }
+    
+    
+}
